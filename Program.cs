e21e8af89abeb9d95bf6924e3b5a25173454e710
William Antunes <william.antunes.mv@gmail.com>
2022-02-12 20:52:58 -0300

    update to net6.0

--- Program.cs
+++ Program.cs
@@ -1,34 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿var characters = new List<DndClass>();
 
-namespace SmartEnums
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var characters = new List<DndClass>();
+characters.Add(DndClass.Barbarian);
+characters.Add(DndClass.Bard);
+characters.Add(DndClass.Cleric);
+characters.Add(DndClass.Druid);
+characters.Add(DndClass.Fighter);
+characters.Add(DndClass.Monk);
+characters.Add(DndClass.Paladin);
+characters.Add(DndClass.Ranger);
+characters.Add(DndClass.Rogue);
+characters.Add(DndClass.Sorcerer);
+characters.Add(DndClass.Warlock);
+characters.Add(DndClass.Wizard);
 
-            characters.Add(DndClass.Barbarian);
-            characters.Add(DndClass.Bard);
-            characters.Add(DndClass.Cleric);
-            characters.Add(DndClass.Druid);
-            characters.Add(DndClass.Fighter);
-            characters.Add(DndClass.Monk);
-            characters.Add(DndClass.Paladin);
-            characters.Add(DndClass.Ranger);
-            characters.Add(DndClass.Rogue);
-            characters.Add(DndClass.Sorcerer);
-            characters.Add(DndClass.Warlock);
-            characters.Add(DndClass.Wizard);
+System.Console.WriteLine("A malignity of Goblins attacks the party! The battle starts:\n");
 
-            System.Console.WriteLine("A malignity of Goblins attacks the party! The battle starts:\n");
+foreach (var character in characters)
+    System.Console.WriteLine($"{character.Value}. {character.Name} {character.Action}");
 
-            foreach (var character in characters)
-                System.Console.WriteLine($"{character.Value}. {character.Name} {character.Action}");
-
-            System.Console.WriteLine("\nThe battle has ended... Victory!");
-
-        }
-    }
-}
+System.Console.WriteLine("\nThe battle has ended... Victory!");
